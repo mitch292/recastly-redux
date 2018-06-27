3644d20b86b0879d11958a15f9a3e13674994d86
Andrew Mitchell <andrewpmitchell7@gmail.com>
2018-06-27 19:18:38 -0400

    Passing all spec runner tests

--- src/containers/VideoPlayerContainer.js
+++ src/containers/VideoPlayerContainer.js
@@ -1,9 +1,37 @@
 import { connect } from 'react-redux';
 import VideoPlayer from '../components/VideoPlayer.js';
 
-var VideoPlayerContainer = () => {};
+
+const mapStateToProps = (state) => ({
+  video: state.currentVideo
+})
+
+const mapDispatchToProps = (dispatch) => {}
+
+
+const VideoPlayerContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(VideoPlayer)
 
 //TODO: define a VideoPlayerContainer component which will hook up your action
 //dispatchers with your VideoPlayer component props.
 
 export default VideoPlayerContainer;
+
+
+
+// const mapStateToProps = (state) => ({
+//   videos: state.videoList
+// })
+
+// const mapDispatchToProps = (dispatch) => ({
+//   handleVideoListEntryTitleClick: (video) => dispatch(changeVideo(video))
+// })
+
+
+
+// const VideoListContainer = connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(VideoList);
